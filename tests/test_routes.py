23d1d16280338eb515d6551d4fd83e9e43193114
Cpt-Opris <opris.alexandru27@gmail.com>
2026-04-01 06:55:53 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,6 +25,8 @@ HTTPS_ENVIRON = {"wsgi.url_scheme": "https"}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -139,12 +141,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_list_account(self):
         """It should List all Accounts"""
@@ -153,7 +153,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -170,7 +169,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["id"], account.id)
         self.assertEqual(data["name"], new_name)
-
 
     def test_delete_account(self):
         """It should Delete an Account"""
@@ -221,4 +219,3 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-
