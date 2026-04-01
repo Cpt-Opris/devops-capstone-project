e9be691155fb276ac87db0fd072d8d04006375b6
Cpt-Opris <opris.alexandru27@gmail.com>
2026-04-01 11:13:57 +0300

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -192,3 +192,8 @@ class TestAccountService(TestCase):
             content_type="application/json",
         )
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
